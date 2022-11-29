fd80b5b040348fe86004784de2619b5085f69e33
mhammad <hammadumer007@gmail.com>
2022-11-29 13:45:13 +0500

    Add allItemsName.spec.js

---
+++ tests/allItemsName.spec.js
@@ -0,0 +1,30 @@
+const {test, expect} = require('@playwright/test')
+
+test('Get All Items Name',async ({page})=>{
+    const pageUrl = 'https://www.saucedemo.com/'
+    const pageTitle= 'Swag Labs'
+    const usernameLocator = page.locator('input[id="user-name"]')
+    const passwordLocator = page.locator('input[id="password"]')
+    const signinBtnLocator = page.locator('input[id="login-button"]')
+    const usernameValue = 'standard_user'
+    const userPasswordValue = 'abcd'
+    const errorMessage = page.locator('h3[data-test="error"]')
+    const dashboardLocator = page.locator('//div[text()="Sauce Labs Backpack"]')
+    const itemsList = page.locator('div[class="inventory_item_name"]')
+
+    
+    await page.goto(pageUrl)
+    await expect(page).toHaveTitle(pageTitle)
+    await usernameLocator.type(usernameValue)
+    await passwordLocator.type(userPasswordValue)
+    await signinBtnLocator.click()
+    await expect(errorMessage).toContainText('Username and password')
+    await passwordLocator.fill("secret_sauce")
+    await signinBtnLocator.click()
+    await expect(dashboardLocator).toHaveText('Sauce Labs Backpack')
+    await page.waitForLoadState('networkidle') // => wait for all the request to complete
+    const itemsName = await itemsList.allTextContents() // will return an array of items name
+    console.log(itemsName)
+
+
+})
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,7 @@
-const {test, expect} = require('@playwright/test')
+const {test, expect} = require('@playwright/test');
+const { timeout } = require('../playwright.config');
 
-test.only('Login with valid credentials', async ({page})=>{
+test('Login with valid credentials', async ({page})=>{
     const pageUrl = 'https://www.saucedemo.com/'
     const pageTitle= 'Swag Labs'
     const usernameLocator = page.locator('input[id="user-name"]')
@@ -30,6 +31,8 @@ test('Login with invalid credentials', async ({page})=>{
     const usernameValue = 'standard_user'
     const userPasswordValue = 'abcd'
     const errorMessage = page.locator('h3[data-test="error"]')
+    const dashboardLocator = page.locator('//div[text()="Sauce Labs Backpack"]')
+
     
     await page.goto(pageUrl)
     await expect(page).toHaveTitle(pageTitle)
@@ -37,5 +40,8 @@ test('Login with invalid credentials', async ({page})=>{
     await passwordLocator.type(userPasswordValue)
     await signinBtnLocator.click()
     await expect(errorMessage).toContainText('Username and password')
+    await passwordLocator.fill("secret_sauce")
+    await signinBtnLocator.click()
+    await expect(dashboardLocator).toHaveText('Sauce Labs Backpack')
 
 })
