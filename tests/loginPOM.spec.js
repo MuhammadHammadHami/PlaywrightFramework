ba1814019052a048fa47b4b2ecc2db51420a5900
mhammad <hammadumer007@gmail.com>
2022-12-11 12:20:49 +0500

    added dataset.json for test cases

--- tests/loginPOM.spec.js
+++ tests/loginPOM.spec.js
@@ -1,32 +1,33 @@
 const {test, expect} = require('@playwright/test');
-const {LoginPage} = require('../pageObjects/LoginPage')
+const {POManager} = require('../pageObjects/POManager')
+const dataset = JSON.parse(JSON.stringify(require('../test-Data/dataset.json')))
 
-test('Login with valid credentials: Sauce Lap', async ({page})=>{
-    const loginPage = new LoginPage(page)
-    const pageUrl = 'https://www.saucedemo.com/'
-    const usernameValue = 'standard_user'
-    const userPasswordValue = 'secret_sauce'
+for (let data of dataset){
+test.only(`Login with ${data.userPasswordValue} password: Sauce Lap'` , async ({page})=>{
+    
+    const poManager = new POManager(page)
+    const loginPage = poManager.getLoginPage()
 
-    await loginPage.goTo(pageUrl)
-    await loginPage.validLogin(usernameValue,userPasswordValue)
+    await loginPage.goTo(data.pageUrl)
+    await expect(page).toHaveTitle(data.pageTitle)
+    await loginPage.validLogin(data.usernameValue,data.userPasswordValue)
+    });
 
-});
+}
 
-test.only('Login with invalid credentials', async ({page})=>{
-    const loginPage = new LoginPage(page)
-    const pageUrl = 'https://www.saucedemo.com/'
-    const pageTitle= 'Swag Labs'
-    const usernameValue = 'standard_user'
-    const userPasswordValue = 'abcd'
+// test('Login with invalid credentials', async ({page})=>{
+    
+//     const poManager = new POManager(page)
+//     const loginPage = poManager.getLoginPage()
   
-    //await expect(page).toHaveTitle(pageTitle)
+//     //await expect(page).toHaveTitle(pageTitle)
     
-    await loginPage.goTo(pageUrl)
-    await expect(page).toHaveTitle(pageTitle)
-    await loginPage.invalidLogin(usernameValue,userPasswordValue)
+//     await loginPage.goTo(dataset.pageUrl)
+//     await expect(page).toHaveTitle(dataset.pageTitle)
+//     await loginPage.invalidLogin(dataset.usernameValue,dataset.userPasswordValue)
     
-    // await passwordLocator.fill("secret_sauce")
-    // await signinBtnLocator.click()
-    // await expect(dashboardLocator).toHaveText('Sauce Labs Backpack')
+//     // await passwordLocator.fill("secret_sauce")
+//     // await signinBtnLocator.click()
+//     // await expect(dashboardLocator).toHaveText('Sauce Labs Backpack')
 
-})
+// })
